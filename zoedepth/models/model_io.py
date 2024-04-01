3931c2954c716c49208fb192bfc845f43a34fe94
nagadomi <nagadomi@gmail.com>
2024-04-01 12:55:22 +0900

    Use weights_only=True for torch.hub.load_state_dict_from_url
    Avoid security risks

--- zoedepth/models/model_io.py
+++ zoedepth/models/model_io.py
@@ -67,7 +67,7 @@ def load_wts(model, checkpoint_path):
 
 
 def load_state_dict_from_url(model, url, **kwargs):
-    state_dict = torch.hub.load_state_dict_from_url(url, map_location='cpu', **kwargs)
+    state_dict = torch.hub.load_state_dict_from_url(url, map_location='cpu', weights_only=True, **kwargs)
     return load_state_dict(model, state_dict)
 
 
